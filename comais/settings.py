f87a6599b7cfc6553303ff0d4c3d5dd4af46c77f
Rogerio Nogueira <roger@uft.edu.br>
2025-03-09 17:36:29 -0300

    home css

--- comais/settings.py
+++ comais/settings.py
@@ -126,9 +126,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "website/static/"),
+    os.path.join(BASE_DIR, "website/static/"), 
     os.path.join(BASE_DIR, "website/static/website"),
 ]
 
