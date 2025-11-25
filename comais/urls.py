c1fecad18a958ddd2fc91093a231ce690157cb1a
rogernogueira <roger@uft.edu.br>
2025-11-25 16:42:08 -0300

    analisededados

--- comais/urls.py
+++ comais/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.i18n import set_language
+from django.views.static import serve
+from django.urls import re_path
 
 
 urlpatterns = [
@@ -14,7 +16,12 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
     path('pdf/', include('pdf.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    # Serve arquivos HTML do diretório analisededados
+    re_path(r'^analisededados/(?P<path>.*)$', serve, {
+        'document_root': settings.BASE_DIR / 'analisededados',
+    }),
+]
+
 admin.site.site_header = 'COMAIS'
 admin.site.site_title = ''
 admin.site.index_title = 'Administração do COMAIS'
